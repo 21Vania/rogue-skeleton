4ff85e662d06105c3770279d188accde97eadb08
21Vania <vania.clesca@mines-paristech.fr>
2022-05-09 12:06:25 +0200

    Update player.py

--- game_backend/player.py
+++ game_backend/player.py
@@ -15,7 +15,7 @@ class Player:
         while found is False:
             y_init += 1
             for i,c in enumerate(_map[y_init]):
-                if c == ".":
+                if c == '.':
                     x_init = i
                     found = True
                     break
@@ -30,20 +30,20 @@ class Player:
         new_y = self._y + dy
 
 # J'ai enlévé les croix que laisse le perso derrière lui qd il avance parce que cétait chiant
-        if map[new_y][new_x] == ".": #or map[new_y][new_x] == "x" :
+        if map[new_y][new_x] == '.': #or map[new_y][new_x] == "x" :
             ret = True
             map[new_y][new_x] = self._symbol
             #map[self._y][self._x] = "x"
             map[self._y][self._x] = "."
             #data = [{"i": f"{self._y}", "j":f"{self._x}", "content":"x"}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":"."}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
             self._x = new_x
             self._y = new_y
         elif map[new_y][new_x] == u'\U0001F4B0' or map[new_y][new_x] == u'\U0001F9EA' or map[new_y][new_x] == u'\U0001F5E1' or map[new_y][new_x] == u'\U0001F49C':
             ret = True
             map[new_y][new_x] = self._symbol
             map[self._y][self._x] = "."
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":"."}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
             self._x = new_x
             self._y = new_y
         else:

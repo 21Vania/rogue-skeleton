18fd413f39aa59f474bfe52a9d17e20bdb50381f
21Vania <vania.clesca@mines-paristech.fr>
2022-05-14 11:22:39 +0200

    Mort des joueurs

--- game_backend/player.py
+++ game_backend/player.py
@@ -33,64 +33,87 @@ class Player:
     def move(self, dx, dy, map):
         new_x = self._x + dx
         new_y = self._y + dy
-        #print("testing move function")
         #print(f"{map[new_y][new_x]=}")
 
-        if map[new_y][new_x] == '.':
-            ret = True
-            map[new_y][new_x] = self._symbol
-            #map[self._y][self._x] = "x"
-            map[self._y][self._x] = "."
-            #data = [{"i": f"{self._y}", "j":f"{self._x}", "content":"x"}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}]
-            self._x = new_x
-            self._y = new_y
-        elif map[new_y][new_x] == 'l':
-            ret = True
-            map[new_y][new_x] = self._symbol
-            map[self._y][self._x] = "."
-            new_life = self.life + 1
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": new_life, "potion": self.potion, "weapon": self.weapon, "money": self.money}]
-            self.life = new_life
-            self._x = new_x
-            self._y = new_y
-        elif map[new_y][new_x] == 'p':
-            ret = True
-            map[new_y][new_x] = self._symbol
-            map[self._y][self._x] = "."
-            new_potion = self.potion + 1
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": self.life, "potion": new_potion, "weapon": self.weapon, "money": self.money}]
-            self.potion = new_potion
-            self._x = new_x
-            self._y = new_y
-        elif map[new_y][new_x] == 'w':
-            ret = True
-            map[new_y][new_x] = self._symbol
-            map[self._y][self._x] = "."
-            new_weapon = self.weapon + 1
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": self.life, "potion": self.potion, "weapon": new_weapon, "money": self.money}]
-            self.weapon = new_weapon
-            self._x = new_x
-            self._y = new_y
-        elif map[new_y][new_x] == '$':
-            ret = True
-            map[new_y][new_x] = self._symbol
-            map[self._y][self._x] = "."
-            new_money = self.money + 1
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": new_money}]
-            self.money = new_money
-            self._x = new_x
-            self._y = new_y
+        if self.life > 0:
+            if map[new_y][new_x] == '.':
+                ret = True
+                alive = True
+                map[new_y][new_x] = self._symbol
+                map[self._y][self._x] = "."
+                data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                self._x = new_x
+                self._y = new_y
+            elif map[new_y][new_x] == 'l':
+                ret = True
+                alive = True
+                map[new_y][new_x] = self._symbol
+                map[self._y][self._x] = "."
+                self.life = self.life + 1
+                data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                self._x = new_x
+                self._y = new_y
+            elif map[new_y][new_x] == 'p':
+                ret = True
+                alive = True
+                map[new_y][new_x] = self._symbol
+                map[self._y][self._x] = "."
+                self.potion = self.potion + 1
+                data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                self._x = new_x
+                self._y = new_y
+            elif map[new_y][new_x] == 'w':
+                ret = True
+                alive = True
+                map[new_y][new_x] = self._symbol
+                map[self._y][self._x] = "."
+                self.weapon = self.weapon + 1
+                data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                self._x = new_x
+                self._y = new_y
+            elif map[new_y][new_x] == '$':
+                ret = True
+                alive = True
+                map[new_y][new_x] = self._symbol
+                map[self._y][self._x] = "."
+                self.money = self.money + 1
+                data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                self._x = new_x
+                self._y = new_y
+            elif map[new_y][new_x] == 'm':
+                if self.weapon > 0:
+                    ret = False
+                    alive = True
+                    self.weapon = self.weapon - 1
+                    data = []
+                    items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                else:
+                    ret = False
+                    alive = True
+                    self.life = self.life - 1
+                    data = []
+                    items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                    if self.life == 0:
+                        ret = False
+                        alive = False
+                        data = []
+                        items = {"life": 0, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+            else:
+                ret = False
+                alive = True
+                data = []
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
         else:
             ret = False
+            alive = False
             data = []
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}]
-        return data, ret, items
+            items = {"life": 0, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+        return data, ret, items, alive
 
 
 class Player2:
@@ -125,68 +148,94 @@ class Player2:
     def move(self, dx, dy, map):
         new_x = self._x + dx
         new_y = self._y + dy
+        #print(f"{map[new_y][new_x]=}")
 
-        if map[new_y][new_x] == '.':
-            ret = True
-            map[new_y][new_x] = self._symbol
-            #map[self._y][self._x] = "x"
-            map[self._y][self._x] = "."
-            #data = [{"i": f"{self._y}", "j":f"{self._x}", "content":"x"}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}]
-            self._x = new_x
-            self._y = new_y
-        elif map[new_y][new_x] == 'l':
-            ret = True
-            map[new_y][new_x] = self._symbol
-            map[self._y][self._x] = "."
-            new_life = self.life + 1
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": new_life, "potion": self.potion, "weapon": self.weapon, "money": self.money}]
-            self.life = new_life
-            self._x = new_x
-            self._y = new_y
-        elif map[new_y][new_x] == 'p':
-            ret = True
-            map[new_y][new_x] = self._symbol
-            map[self._y][self._x] = "."
-            new_potion = self.potion + 1
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": self.life, "potion": new_potion, "weapon": self.weapon, "money": self.money}]
-            self.potion = new_potion
-            self._x = new_x
-            self._y = new_y
-        elif map[new_y][new_x] == 'w':
-            ret = True
-            map[new_y][new_x] = self._symbol
-            map[self._y][self._x] = "."
-            new_weapon = self.weapon + 1
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": self.life, "potion": self.potion, "weapon": new_weapon, "money": self.money}]
-            self.weapon = new_weapon
-            self._x = new_x
-            self._y = new_y
-        elif map[new_y][new_x] == '$':
-            ret = True
-            map[new_y][new_x] = self._symbol
-            map[self._y][self._x] = "."
-            new_money = self.money + 1
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": new_money}]
-            self.money = new_money
-            self._x = new_x
-            self._y = new_y
+        if self.life > 0:
+            if map[new_y][new_x] == '.':
+                ret = True
+                alive = True
+                map[new_y][new_x] = self._symbol
+                map[self._y][self._x] = "."
+                data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                self._x = new_x
+                self._y = new_y
+            elif map[new_y][new_x] == 'l':
+                ret = True
+                alive = True
+                map[new_y][new_x] = self._symbol
+                map[self._y][self._x] = "."
+                self.life = self.life + 1
+                data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                self._x = new_x
+                self._y = new_y
+            elif map[new_y][new_x] == 'p':
+                ret = True
+                alive = True
+                map[new_y][new_x] = self._symbol
+                map[self._y][self._x] = "."
+                self.potion = self.potion + 1
+                data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                self._x = new_x
+                self._y = new_y
+            elif map[new_y][new_x] == 'w':
+                ret = True
+                alive = True
+                map[new_y][new_x] = self._symbol
+                map[self._y][self._x] = "."
+                self.weapon = self.weapon + 1
+                data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                self._x = new_x
+                self._y = new_y
+            elif map[new_y][new_x] == '$':
+                ret = True
+                alive = True
+                map[new_y][new_x] = self._symbol
+                map[self._y][self._x] = "."
+                self.money = self.money + 1
+                data = [{"i": f"{self._y}", "j":f"{self._x}", "content":'.'}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                self._x = new_x
+                self._y = new_y
+            elif map[new_y][new_x] == 'm':
+                if self.weapon > 0:
+                    ret = False
+                    alive = True
+                    self.weapon = self.weapon - 1
+                    data = []
+                    items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                else:
+                    ret = False
+                    alive = True
+                    self.life = self.life - 1
+                    data = []
+                    items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+                    if self.life == 0:
+                        ret = False
+                        alive = False
+                        data = []
+                        items = {"life": 0, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+            else:
+                ret = False
+                alive = True
+                data = []
+                items = {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}
         else:
             ret = False
+            alive = False
             data = []
-            items = [{"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}, {"life": self.life, "potion": self.potion, "weapon": self.weapon, "money": self.money}]
-        return data, ret, items
+            items = {"life": 0, "potion": self.potion, "weapon": self.weapon, "money": self.money}
+        return data, ret, items, alive
 
 class Monster:
     def __init__(self, symbol='m'):
         self._symbol = symbol
         self._x = None
         self._y = None
+        self.life = 1
 
     def initPos(self, _map):
         y_init = random.randint(1, len(_map) - 1)
